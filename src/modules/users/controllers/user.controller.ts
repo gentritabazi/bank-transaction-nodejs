490ba005c3fecc22b8e6ba841e71e1e0b3c83682
Gentrit Abazi <gentritabazi01@gmail.com>
2023-07-07 17:47:21 +0200

    Remove unused import

--- src/modules/users/controllers/user.controller.ts
+++ src/modules/users/controllers/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, HttpStatus } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 import { CreateUserDto } from '../dto/create-user.dto';
 
